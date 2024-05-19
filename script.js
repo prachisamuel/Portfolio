6e43012639445de190921f367e86f388f42d679b
Prachi Samuel <prachisamuel28@gmail.com>
2024-05-20 03:12:12 +0530

    new project added

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".carousel").scrollLeft = 0;
+    const carousel = document.querySelector(".carousel");
+    const prevBtn = document.querySelector(".arrow-left");
+    const nextBtn = document.querySelector(".arrow-right");
+
+    const scrollWidth = carousel.scrollWidth;
+    const clientWidth = carousel.clientWidth;
+
+    let scrollPosition = 0;
+
+    // Move carousel to the left
+    prevBtn.addEventListener("click", function () {
+        if (scrollPosition > 0) {
+        scrollPosition -= clientWidth;
+        carousel.scrollTo({
+            left: scrollPosition,
+            behavior: "smooth",
+        });
+        }
+    });
+
+    // Move carousel to the right
+    nextBtn.addEventListener("click", function () {
+        if (scrollPosition + clientWidth < scrollWidth) {
+        scrollPosition += clientWidth;
+        carousel.scrollTo({
+            left: scrollPosition,
+            behavior: "smooth",
+        });
+        }
+    });
+});
+  
